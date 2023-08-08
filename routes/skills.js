9ac623f57fda24dcb6ee2a4a8ba9b59fa9911b7e
MNieginski <mikalnieginski@gmail.com>
2023-08-07 23:50:22 -0400

    update skills

--- routes/skills.js
+++ routes/skills.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 // Require the controller that exports To-Do CRUD functions
-var todosCtrl = require('../controllers/todos');
+var skillsCtrl = require('../controllers/skills');
 
 
 // All actual paths start with "/todos"
 
 // GET /todos
-router.get('/', todosCtrl.index);
+router.get('/', skillsCtrl.index);
 // GET /todos/:id
-router.get('/:id', todosCtrl.show);
+router.get('/:id', skillsCtrl.show);
 
 module.exports = router;
